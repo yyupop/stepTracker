8ce1b90c4b528e6f905aa8ee878761f8cbecd36b
nixanok <mrfreemen555@gmail.com>
2024-02-20 20:25:50 +0300

    замечания

--- src/Converter.java
+++ src/Converter.java
@@ -2,11 +2,15 @@
 import java.util.Scanner;
 public class Converter {
     int convertToKm(int steps) {
+        // Если подсчеты реализованы в одну строку,
+        // можно не создавать дополнительную переменную, а сразу возвращать
+        // То есть сразу return steps * 75 / 10000;
         int distance = steps * 75 / 10000;
         return distance;
     }
 
     int convertStepsToKilocalories(int steps) {
+        // Здесь аналогично
         int kilocalories = steps * 50 / 1000;
         return kilocalories;
     }
